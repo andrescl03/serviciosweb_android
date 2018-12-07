2cb7b3849e0b34880017066a8c355ed5d330464b
andrescl03 <andrescarrion199603@gmail.com>
2018-12-07 05:16:28 -0500

    creacion del panel admin

--- app/src/main/java/r/andres/serviciosweb_android/DetallePanel.java
+++ app/src/main/java/r/andres/serviciosweb_android/DetallePanel.java
@@ -2,17 +2,73 @@ package r.andres.serviciosweb_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 
 import r.andres.serviciosweb_android.entidades.usuarioBean;
 
-public class DetallePanel extends AppCompatActivity {
+public class DetallePanel extends AppCompatActivity implements View.OnClickListener , Response.Listener<String>, Response.ErrorListener{
 
+    Button btnDAgregarP;
+    Button btnDGrabar;
+    TextView txtDUsuario;
+    EditText txtDIdentificador,txtDDescripcion,txtDPreguntas;
+    TableLayout tableDPreguntas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_panel);
 
+        llamadaRecursos();
+        btnDGrabar.setOnClickListener(this);
+        btnDAgregarP.setOnClickListener(this);
+
     }
 
 
+
+    public void llamadaRecursos(){
+        btnDAgregarP = findViewById(R.id.btnDAgregarP);
+        btnDGrabar = findViewById(R.id.btnDGrabar);
+        txtDUsuario = findViewById(R.id.txtDUsuario);
+        txtDIdentificador  = findViewById(R.id.txtDIdentificador);
+        txtDDescripcion = findViewById(R.id.txtDDescripcion);
+        txtDPreguntas = findViewById(R.id.txtDPreguntas);
+        tableDPreguntas = findViewById(R.id.tableDPreguntas);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnDAgregarP:
+                break;
+
+
+            case R.id.btnDGrabar:
+                break;
+        }
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(String response) {
+
+    }
+
+    public void cargarWebService(){
+        
+    }
 }
